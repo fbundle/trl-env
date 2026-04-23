e700212595507885bef2a5f56d096ac07c198ace
Nguyen Ngoc Khanh <nguyenng@a2ap-login02.asp2p.nscc.sg>
2026-04-23 17:00:09 +0800

    auto_commit

---
+++ experiment/upload_huggingface.py
@@ -0,0 +1,25 @@
+import os
+import sys
+
+from huggingface_hub import login, upload_large_folder
+from dotenv import load_dotenv
+
+def upload(output_dir: str):
+    load_dotenv()
+    hf_user = os.environ.get("HF_USER", None)
+    if hf_user is None:
+        raise RuntimeError("HF_USER must be set")
+
+    repo_id = hf_user + "/" + os.path.basename(output_dir)
+
+    login()
+    upload_large_folder(
+        folder_path=output_dir,
+        repo_id=repo_id,
+        repo_type="model",
+        # ignore_patterns=["checkpoint-*"],
+    )
+
+
+if __name__ == "__main__":
+    upload(sys.argv[1])
