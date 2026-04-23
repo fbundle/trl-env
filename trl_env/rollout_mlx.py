f5c1c8f7c7de05d252fdeb8ce2b4585f955365f6
nguyen ngoc khanh <khanh.nguyen.contact@gmail.com>
2026-04-24 00:00:44 +0800

    auto_commit

--- trl_env/rollout_mlx.py
+++ trl_env/rollout_mlx.py
@@ -1,28 +1,36 @@
 
 
 
-from typing import Union
+from typing import Union, Any, Callable
 
 import mlx.nn as nn
 import mlx_lm
-from mlx_lm.tokenizer_utils import TokenizerWrapper
 import mlx.core as mx
-from transformers import AutoTokenizer, PreTrainedTokenizerBase
+from transformers import AutoTokenizer, PreTrainedModel, PreTrainedTokenizerBase
 
 from trl_env.engine import Engine
 
+from trl.trainer.grpo_trainer import RolloutFunc, GRPOTrainer, RewardFunc
+
+from trl_env.environment import Env
+from trl_env.processor import Processor
+from trl_env.rollout import batch_rollout
+
 
 class MlxEngine(Engine):
-    def __init__(self, model_path: str) -> None:
+    def __init__(self, model_path: str, transformer_model: PreTrainedModel) -> None:
         model, _, _ = mlx_lm.load(  # type: ignore
             path_or_hf_repo=model_path,
             return_config=True,
         )
         tokenizer = AutoTokenizer.from_pretrained(model_path)
+        self.transformer_model = transformer_model
         self.model: nn.Module = model
         self.tokenizer: PreTrainedTokenizerBase = tokenizer
     
-    def load_weights(self, file_or_weights: Union[str, list[tuple[str, mx.array]]]):
+    def update_weights(self):
+        # TODO - get self.transformer_model weights
+        file_or_weights: Union[str, list[tuple[str, mx.array]]] = None # type: ignore
         self.model.load_weights(file_or_weights)
 
     def tokenizer_encode(self, input_text: str) -> list[int]:
@@ -38,3 +46,32 @@ class MlxEngine(Engine):
         ...
 
 
+def make_rollout_func(
+    engine: MlxEngine, processor: Processor, env_factory: Callable[[], Env],
+    system_prompt: str, max_conversation_length: int,
+) -> RolloutFunc:
+    def rollout_func(prompts: list[str], trainer: GRPOTrainer) -> dict[str, Any]:
+        try:
+            engine.update_weights()
+            state_list = batch_rollout(
+                engine=engine, processor=processor, env_factory=env_factory,
+                system_prompt=system_prompt, max_conversation_length=max_conversation_length,
+                seed_list=prompts,
+            )
+        finally:
+            ...
+        
+        return {
+            "prompt_ids": [state.conversation[:state.initial_length] for state in state_list],
+            "completion_ids": [state.conversation[state.initial_length:] for state in state_list],
+            "env_mask": [state.env_mask for state in state_list],
+            "logprobs": [state.logprobs for state in state_list],
+            "reward": [state.reward for state in state_list],
+        }
+
+    return rollout_func
+
+def make_reward_func() -> RewardFunc:
+    def reward_func(prompts: list[str], completions: list[str], reward: list[float], **kwargs) -> list[float]:
+            return reward
+    return reward_func # type: ignore
