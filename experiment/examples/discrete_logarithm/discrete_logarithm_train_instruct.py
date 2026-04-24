8c249f04259b87bc00a0229c1e8d548451eecda9
nguyen ngoc khanh <khanh.nguyen.contact@gmail.com>
2026-04-25 03:36:53 +0800

    auto_commit

--- experiment/examples/discrete_logarithm/discrete_logarithm_train_instruct.py
+++ experiment/examples/discrete_logarithm/discrete_logarithm_train_instruct.py
@@ -119,10 +119,10 @@ def load_batch_information(mode: Mode):
     # alpha = 2 for usual transformer
     # alpha = 1 for flash attention
     effective_batch_size = 16
-    per_device_batch_size = 2
+    per_device_batch_size = 4
     num_generations = 8
     max_conversation_length = 4096
-    max_turn_length = 2048
+    max_turn_length = 1024
 
     if mode == ModeDebug:
         effective_batch_size = 4
