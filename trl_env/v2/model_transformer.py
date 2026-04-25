b674bcd3a9965288798b98116d47b56821cd8789
khanh <khanh.nguyen.contact@gmail.com>
2026-04-25 23:32:56 +0800

    auto_commit

--- trl_env/v2/model_transformer.py
+++ trl_env/v2/model_transformer.py
@@ -5,10 +5,10 @@ import torch
 from transformers import Cache
 
 from trl_env.v2.generate import StreamGenerationIteration, Token, stream_generate
-from trl_env.v2.model import RolloutModel
+from trl_env.v2.model import RolloutModelContext
 from trl_env.v2.generate_transformer import BaseModelWithGenerate, make_model_func, make_sample_func
 
-class TransformerRolloutModel(RolloutModel):
+class TransformerRolloutModelContext(RolloutModelContext):
     def __init__(self,
     model: BaseModelWithGenerate,
     temperature: float,
@@ -17,14 +17,18 @@ class TransformerRolloutModel(RolloutModel):
 ) -> None:
         self.model: BaseModelWithGenerate = model
         self.state: Cache | None = None
+        self.last_length = 0
 
         self.temperature = temperature
         self.eos_token_set = eos_token_set
         self.max_completion_length = max_completion_length
     
     def generate(self, input_ids: list[int]) -> tuple[list[int], list[float]]
+        new_input_ids = input_ids[self.last_length:]
+        
+
         i: Iterator[StreamGenerationIteration[Cache | None]] = stream_generate(
-            new_token_list=torch.tensor(input_ids),
+            new_token_list=torch.tensor(new_input_ids),
             prev_state=self.state,
             model_func=make_model_func(model=self.model),
             sample_func=make_sample_func(temperature=self.temperature),
@@ -44,5 +48,7 @@ class TransformerRolloutModel(RolloutModel):
             new_state = o.state
         
         self.state = new_state
+        self.last_length = len(input_ids) + len(completion_token_list)
+
         
         return completion_token_list, logprob_list
