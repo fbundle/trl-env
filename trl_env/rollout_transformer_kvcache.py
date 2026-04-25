b87a238884590f9cce582b6a8ea46639ad76b3cb
khanh <khanh.nguyen.contact@gmail.com>
2026-04-25 20:04:45 +0800

    auto_commit

--- trl_env/rollout_transformer_kvcache.py
+++ trl_env/rollout_transformer_kvcache.py
@@ -1,14 +1,20 @@
 import itertools
-from typing import Callable, Generator
+from typing import Any, Callable, Generator
 
 import torch
 from torch import Tensor
 from jaxtyping import Float, Int
 
-from transformers import Cache, GenerationMixin, PreTrainedModel
+from transformers import Cache, GenerationMixin, PreTrainedModel, PreTrainedTokenizerBase
 from transformers.modeling_outputs import CausalLMOutputWithPast
 
-from trl_env.processor import *
+from trl_env.engine import Engine
+from trl_env.environment import Env
+
+from trl.trainer.grpo_trainer import RolloutFunc, GRPOTrainer, RewardFunc
+
+from trl_env.processor import Processor
+from trl_env.rollout import batch_rollout
 
 class _BaseModelWithGenerate(PreTrainedModel, GenerationMixin):
     pass
@@ -70,26 +76,113 @@ class State[T]:
         self._stop_cond = stop_cond
         self._cache = cache
     
-    def generate(self, input_tokens: list[int]) -> Generator[tuple[int, Logits], None, None]:
+    def generate(self, input_tokens: Int[Tensor, "n"]) -> Generator[tuple[int, Logits], None, None]:
         token = None
         try:
             for turn in itertools.count():
                 # generate
-                self._cache, logits = self._generate(self._cache, torch.tensor(input_tokens))
+                self._cache, logits = self._generate(self._cache, input_tokens)
                 # sample
                 token = self._sampler(logits)
                 yield token, logits
                 if self._stop_cond(turn, token):
                     break
                 # prepare next generate
-                input_tokens = [token]
+                input_tokens = torch.tensor([token])
         finally:
             # finally, put the last token into the cache
             if token is not None:
                 self._cache, _ = self._generate(self._cache, torch.tensor([token]))
 
+class TransformerEngine(Engine):
+    def __init__(self,
+        tokenizer: PreTrainedTokenizerBase,
+        model: _BaseModelWithGenerate,
+        temperature: float = 0.6,
+        max_completion_length: int = 512,
+    ) -> None:
+        self.tokenizer = tokenizer
+        self.model = model
+        self.temperature = temperature
+        self.max_completion_length = max_completion_length
+
+        self.states: list[State[Cache | None]] = []
+    
+    def tokenizer_encode(self, input_text: str) -> list[int]:
+        return self.tokenizer(input_text).input_ids
+
+    def tokenizer_decode(self, completion_ids: list[int]) -> str:
+        output_text = self.tokenizer.decode(completion_ids)
+        assert isinstance(output_text, str)
+        return output_text
+    
+    def reset_states(self, n: int):
+        self.states = [State(
+            generate=make_model_generate(self.model),
+            sampler=make_sampler(self.temperature),
+            stop_cond=make_stop_cond(
+                eos_token_set={self.tokenizer.eos_token_id},    # type: ignore
+                max_completion_length=self.max_completion_length,
+            )
+        ) for _ in range(n)]
+
+    def model_batch_generate(self, input_ids_list: list[list[int]]) -> tuple[list[list[int]], list[list[float]]]:
+        completion_ids_list = []
+        logprobs_list = []
+        for i, input_tokens in enumerate(input_ids_list):
+            completion_ids = []
+            logprobs = []
+            assert len(self.states) == len(input_ids_list)
+            assert state is not None
+            for token, logits in self.states[i].generate(torch.tensor(input_tokens)):
+                completion_ids.append(token)
+                dist: Float[Tensor, "d"] = torch.log_softmax(logits, dim=-1)
+                logprob: float = float(dist[token].item())
+                logprobs.append(logprob)
+            
+            completion_ids_list.append(completion_ids)
+            logprobs_list.append(logprobs)
+        
+        return completion_ids_list, logprobs_list
+
+
+def make_rollout_func(
+    engine: TransformerEngine, processor: Processor, env_factory: Callable[[], Env],
+    system_prompt: str, max_conversation_length: int,
+) -> RolloutFunc:
+    def rollout_func(prompts: list[str], trainer: GRPOTrainer) -> dict[str, Any]:
+        try:
+            # NOTE - only rollout on eval mode
+            engine.model.eval()
+            with torch.no_grad():
+                state_list = batch_rollout(
+                    engine=engine, processor=processor, env_factory=env_factory,
+                    system_prompt=system_prompt, max_conversation_length=max_conversation_length,
+                    seed_list=prompts,
+                )
+        finally:
+            engine.model.train()
+        
+        return {
+            "prompt_ids": [state.conversation[:state.initial_length] for state in state_list],
+            "completion_ids": [state.conversation[state.initial_length:] for state in state_list],
+            "env_mask": [state.env_mask for state in state_list],
+            "logprobs": [state.logprobs for state in state_list],
+            "reward": [state.reward for state in state_list],
+        }
+
+    return rollout_func    
+
+def make_reward_func() -> RewardFunc:
+    def reward_func(prompts: list[str], completions: list[str], reward: list[float], **kwargs) -> list[float]:
+            return reward
+    return reward_func # type: ignore
+
+
+
 if __name__ == "__main__":
     from transformers import AutoTokenizer, AutoModelForCausalLM
+    from trl_env.processor import *
     device = "mps"
     model_path = "Qwen/Qwen3.5-0.8B"
 
@@ -110,12 +203,12 @@ if __name__ == "__main__":
     text1 = qwen3_instruct_processor.append_user_input("the cat is lying on a table")
     input_token_list1: list[int] = tokenizer(text1).input_ids
 
-    completion_list1 = list(list(zip(*state.generate(input_token_list1)))[0])
+    completion_list1 = list(list(zip(*state.generate(torch.tensor(input_token_list1))))[0])
 
     text2 = qwen3_instruct_processor.append_user_input("what is the cat lying on?")
     input_token_list2: list[int] = tokenizer(text2).input_ids
     
-    completion_list2 = list(list(zip(*state.generate(input_token_list2)))[0])
+    completion_list2 = list(list(zip(*state.generate(torch.tensor(input_token_list2))))[0])
 
     token_list = input_token_list1 + completion_list1 + input_token_list2 + completion_list2
 
