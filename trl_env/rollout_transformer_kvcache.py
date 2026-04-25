f35b93b3a4c5aa51b307e9535fea9b2123f0a207
nguyen ngoc khanh <khanh.nguyen.contact@gmail.com>
2026-04-25 17:00:05 +0800

    auto_commit

--- trl_env/rollout_transformer_kvcache.py
+++ trl_env/rollout_transformer_kvcache.py
@@ -1,22 +1,19 @@
-from typing import Generator, Literal
+from typing import Generator, Any
+
+from dataclasses import dataclass
 
 import torch
 from torch import Tensor
 from jaxtyping import Float, Int
 
-from transformers import Cache, dataclass
+from transformers import Cache, GenerationMixin, PreTrainedModel
 from transformers.modeling_outputs import CausalLMOutputWithPast
-from transformers.models.auto.modeling_auto import _BaseModelWithGenerate
+# from transformers.models.auto.modeling_auto import _BaseModelWithGenerate
 
+
+class _BaseModelWithGenerate(PreTrainedModel, GenerationMixin):
+    pass
 type Token = int
-
-def get_last_token(token_list: list[Token]) -> Token | None:
-    if len(token_list) == 0:
-        return None
-    else:
-        return token_list[-1]
-
-type FinishReason = Literal["length", "stop", None]
 
 @dataclass
 class GenerateIteration:
@@ -27,7 +24,7 @@ class TransformerEngine:
     def __init__(self, model: _BaseModelWithGenerate):
         self.model = model
     
-    def generate(
+    def stream_generate(
         self, input_token_list: list[Token],
         eos_token_set: set[Token],
         max_completion_length: int = 256,
@@ -37,7 +34,7 @@ class TransformerEngine:
         input_token_queue: list[Token] = input_token_list
 
         with torch.no_grad():
-            for turn in range(max_completion_length):
+            for _ in range(max_completion_length):
                 # batch_size b = 1
                 input_ids: Int[Tensor, "b n"] = torch.tensor([input_token_queue], device=self.model.device)
                 o: CausalLMOutputWithPast = self.model.__call__(
@@ -56,8 +53,12 @@ class TransformerEngine:
 
                 # sample token
                 # x[[a, b, c], [A, B, C]] = [x[a, A], x[b, B], x[c, C]]
-                scaled_probs: Float[Tensor, "b d"] = torch.softmax(logits / temperature, dim=-1)
-                sampled_tokens: Int[Tensor, "b"] = torch.multinomial(scaled_probs, num_samples=1).squeeze(dim=-1)
+                if temperature > 0:
+                    scaled_probs: Float[Tensor, "b d"] = torch.softmax(logits / temperature, dim=-1)
+                    sampled_tokens: Int[Tensor, "b"] = torch.multinomial(scaled_probs, num_samples=1).squeeze(dim=-1)
+                else:
+                    sampled_tokens: Int[Tensor, "b"] = torch.argmax(logits, dim=-1)
+
 
                 # get original logprobs
                 logprobs: Float[Tensor, "b d"] = torch.log_softmax(logits, dim=-1)
@@ -93,3 +94,66 @@ class TransformerEngine:
                 sample=None,
                 past_key_values=o.past_key_values,
             )
+
+    def generate(self, *args, **kwargs) -> tuple[list[tuple[Token, float]], Cache | None]:
+        sample_list = []
+        past_key_values = None
+
+        for i in self.stream_generate(*args, **kwargs):
+            if i.sample is not None:
+                sample_list.append(i.sample)
+            past_key_values = i.past_key_values
+        
+        return sample_list, past_key_values
+        
+        
+
+if __name__ == "__main__":
+    from transformers import AutoTokenizer, AutoModelForCausalLM
+    device = "mps"
+    model_path = "Qwen/Qwen3.5-0.8B"
+
+    tokenizer = AutoTokenizer.from_pretrained(model_path)
+    model: _BaseModelWithGenerate = AutoModelForCausalLM.from_pretrained(model_path).to(device) #type: ignore
+    engine = TransformerEngine(model=model)
+
+    past_key_values = None
+
+    text1 = "my pen is purple"
+    input_token_list1: list[Token] = tokenizer.apply_chat_template( # type: ignore
+        [{"role": "user", "content": text1}],
+        tokenize=True,
+        add_generation_prompt=True,
+        enable_thinking=False,
+    ).input_ids
+
+    sample_list1, past_key_values = engine.generate(
+        input_token_list=input_token_list1,
+        eos_token_set={tokenizer.eos_token_id},
+        past_key_values=past_key_values,
+    )
+
+
+    text2 = "what is the color of my pen again?"
+    input_token_list2: list[Token] = tokenizer.apply_chat_template( # type: ignore
+        [{"role": "user", "content": text2}],
+        tokenize=True,
+        add_generation_prompt=True,
+        enable_thinking=False,
+    ).input_ids
+    
+    sample_list2, past_key_values = engine.generate(
+        input_token_list=input_token_list2,
+        eos_token_set={tokenizer.eos_token_id},
+        past_key_values=past_key_values,
+    )
+
+    completion_list1: list[Token] = list(list(zip(*sample_list1))[0]) # type: ignore
+    completion_list2: list[Token] = list(list(zip(*sample_list2))[0]) # type: ignore
+
+
+    token_list = input_token_list1 + completion_list1 + input_token_list2 + completion_list2
+
+    text = tokenizer.decode(token_list)
+    print(text)
+
