69c75a1f4cb1657876747a67696c62e73f599d1b
khanh <khanh.nguyen.contact@gmail.com>
2026-04-27 20:32:09 +0800

    auto_commit

--- trl_env/decoder_vllm.py
+++ trl_env/decoder_vllm.py
@@ -12,7 +12,7 @@ class VLLMRolloutDecoder(RolloutDecoder):
         self.llm = LLM(
             model=model_path,
             dtype="bfloat16",
-            gpu_memory_utilization=0.4,
+            gpu_memory_utilization=0.2,
             enable_prefix_caching=True,
         )
         self.sampling_params = SamplingParams(
@@ -36,7 +36,46 @@ class VLLMRolloutDecoder(RolloutDecoder):
         )
         assert len(o_list) == 1
         o = o_list[0]
-        tokens = o.outputs[0].token_ids
+        output_ids = o.outputs[0].token_ids
         logprobs = [list(lp.values())[0].logprob for lp in o.outputs[0].logprobs]
-        return tokens, logprobs
+        return output_ids, logprobs
+
+if __name__ == "__main__":
+    from typing import Iterable
+    from transformers import AutoTokenizer, AutoModelForCausalLM
+
+    from .tokenizer import TransformerTokenizer
+    from .processor import *
+
+    device = "cpu"
+    model_path = "Qwen/Qwen3.5-0.8B"
+
+    t = AutoTokenizer.from_pretrained(model_path)
+    m: PreTrainedModel = AutoModelForCausalLM.from_pretrained(model_path).to(device) #type: ignore
+
+    eos_token: int = t.eos_token_id
+
+
+    tokenizer = TransformerTokenizer(t)
+    processor = qwen3_processor
+
+    decoder = VLLMRolloutDecoder(
+        model_path=model_path,
+        temperature=1.0,
+        eos_token_set={eos_token},
+        max_completion_length=512,
+    )
+
+    decoder.update_weights(m)
+
+    input_ids = tokenizer.encode(processor.append_user_input("the cat is lying on the rooftop"))
+    output_ids, logprobs = decoder.generate(input_ids)
+
+    output = tokenizer.decode(output_ids)
+    print(logprobs)
+    print(output)
+
+
+    
+
 
