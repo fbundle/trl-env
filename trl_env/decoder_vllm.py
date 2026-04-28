93367f9bf6b4b748b0a0b4caacec030fbe222dc7
khanh <khanh.nguyen.contact@gmail.com>
2026-04-28 15:19:56 +0800

    auto_commit

--- trl_env/decoder_vllm.py
+++ trl_env/decoder_vllm.py
@@ -26,13 +26,15 @@ class VLLMDecoderFactory(RolloutDecoderFactory):
         temperature: float,
         eos_token_set: set[int],
         max_completion_length: int,
-        gpu_memory_utilization: float = 0.5
+        gpu_memory_utilization: float = 0.5,
+        enable_sleep_mode: bool = True,
     ) -> None:
         self.vllm: VLLMGeneration | None = None
         self.temperature = temperature
         self.eos_token_set = eos_token_set
         self.max_completion_length = max_completion_length
         self.gpu_memory_utilization = gpu_memory_utilization
+        self.enable_sleep_mode = enable_sleep_mode
     
     def make_decoder(self, trainer: Trainer) -> RolloutDecoder:
         if self.vllm is None:
@@ -43,6 +45,7 @@ class VLLMDecoderFactory(RolloutDecoderFactory):
                 processing_class=trainer.processing_class, # type: ignore
                 mode="colocate",
                 gpu_memory_utilization=self.gpu_memory_utilization,
+                enable_sleep_mode=self.enable_sleep_mode,
                 max_completion_length=self.max_completion_length,
                 temperature=self.temperature,
                 generation_kwargs=dict(
