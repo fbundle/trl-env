be8af59d6a9019c68a8373b34c51233e7cbfb870
Nguyen Ngoc Khanh <nguyenng@a2ap-login02.asp2p.nscc.sg>
2026-04-28 15:54:15 +0800

    auto_commit

--- experiment/upload_huggingface.py
+++ experiment/upload_huggingface.py
@@ -1,23 +1,33 @@
 import os
 import sys
 
-from huggingface_hub import login, upload_large_folder
+from huggingface_hub import HfApi, login, upload_large_folder
 from dotenv import load_dotenv
 
+load_dotenv()
+
+def get_hf_info(output_dir: str) -> tuple[bool, str, str]:
+    hf_user = os.environ.get("HF_USER", default=None)
+    hf_token = os.environ.get("HF_TOKEN", default=None)
+    if hf_user is None or hf_token is None:
+        return False, "", ""
+    
+    hf_model = hf_user + "/" + os.path.basename(output_dir)
+    return True, hf_model, hf_token
+
 def upload(output_dir: str):
-    load_dotenv()
-    hf_user = os.environ.get("HF_USER", None)
-    if hf_user is None:
-        raise RuntimeError("HF_USER must be set")
+    push_to_hub, hf_model, hf_token = get_hf_info(output_dir)
 
-    repo_id = hf_user + "/" + os.path.basename(output_dir)
+    if not push_to_hub:
+        raise RuntimeError("HF_USER and HF_TOKEN must be set")
+        return
 
-    login()
-    upload_large_folder(
+    api = HfApi(token=hf_token)
+    api.upload_large_folder(
         folder_path=output_dir,
-        repo_id=repo_id,
+        repo_id=hf_model,
         repo_type="model",
-        # ignore_patterns=["checkpoint-*"],
+        ignore_patterns=["checkpoint-*"],
     )
 
 
