ce0144caaf60bc65efbe8199828e186d2871b71c
khanh <khanh.nguyen.contact@gmail.com>
2026-04-29 13:07:58 +0800

    auto_commit

--- experiment/start_pbs_test.sh
+++ experiment/start_pbs_test.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
+JOB_NAME="log_$1"
+
 source .env
 if [[ -v PBS_PROJECT ]]; then
-    echo "sleep 86400" | qsub -P $PBS_PROJECT -q normal -l select=1:ngpus=1 -l walltime=23:50:00 
+    echo "sleep 86400" | qsub -N $JOB_NAME -P $PBS_PROJECT -q normal -l select=1:ngpus=1 -l walltime=23:50:00 
 else
     echo "PBS_PROJECT not set"
 fi
