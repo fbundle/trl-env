59fe4087de457727c1efa7191ef50d42a52bff8f
khanh <khanh.nguyen.contact@gmail.com>
2026-04-29 14:18:28 +0800

    auto_commit

--- experiment/examples/discrete_logarithm/discrete_logarithm_train_8192.py
+++ experiment/examples/discrete_logarithm/discrete_logarithm_train_8192.py
@@ -179,6 +179,7 @@ def load_env_and_data(effective_batch_size: int):
         proportion: float = i / train_size
         expected_bit_size: float = MIN + (MAX - MIN) * proportion
         p_seed: int = np.random.geometric(1 / 2 ** expected_bit_size)
+        p_seed = max(3, p_seed) # p >= 3
         return generate_seed(p_seed)
     
     data = LazyDataset[str](n=train_size, f=f)
